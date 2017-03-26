9e3c04ac02a88f362d11395c2d596f174e23566e
yann <me.yan.xia@qq.com>
2017-03-26 23:13:54 +0800

    Init

---
+++ desktop/src/info/yannxia/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package info.yannxia.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import info.yannxia.game.HitGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HitGame(), config);
+	}
+}
